91be702f72d547b811d29273aa02fc71196f3488
Aaron Janes <aaron.janes@gmail.com>
2023-07-30 19:56:14 -0600

    camera config works

--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
+
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 
-
 using namespace cv;
 using namespace std;
 
-
-/////////////////  Images  //////////////////////
+///////////////// Webcam //////////////////////
 
 int main() {
 
-    string path = "../Resources/test.png";
-    Mat img = imread(path);
-    imshow("Image", img);
-    waitKey(0);
+    VideoCapture cap(1);
+    Mat img;
+
+    while (true) {
+
+        cap.read(img);
+        imshow("Image", img);
+        waitKey(1);
+
+    }
     return 0;
 }
